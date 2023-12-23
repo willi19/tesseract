772b35ad571f2028c765babd02dca498b9be6911
willi19 <willi19@naver.com>
2023-12-23 16:31:52 +0900

    add communication

--- src/extrinsic_calibrater.py
+++ src/extrinsic_calibrater.py
@@ -1,6 +1,3 @@
-import open3d as o3d
-
-import argparse
 import os
 
 import numpy as np
@@ -9,6 +6,11 @@ import time
 import cv2
 from pyk4a import PyK4A, connected_device_count, Config
 from .utils import convert_to_bgra_if_required
+import threading
+import asyncio
+import json
+import shutil
+from aiohttp import FormData
 
 class ExtrinsicCalibrater:
     def set_config(self, config_path):
@@ -16,6 +18,7 @@ class ExtrinsicCalibrater:
         data = json.load(config_file)
         self.config = Config(**data)
 
+    
     def __init__(self, mode, name):
         self.flag_exit = False
 
@@ -23,6 +26,7 @@ class ExtrinsicCalibrater:
         self.set_config(config_path)
 
         self.devices = [PyK4A(config = self.config, device_id=device_ind) for device_ind in range(4)]
+        self.ids = [device.serial for device in self.devices]
         
         for device in self.devices:
             device.start()
@@ -38,77 +42,69 @@ class ExtrinsicCalibrater:
         self.dirname = os.path.join('data','extrinsic',self.name)
 
         shutil.copyfile(config_path, os.path.join(self.dirname, 'config.json'))
-        os.makedirs(self.dirname, exist_ok=True)                        
+        os.makedirs(self.dirname, exist_ok=True)
+        
+        self.input_thread = threading.Thread(target=self.run)                        
 
-    def escape_callback(self, vis):
+    def thread_input(self):
+        while not self.flag_exit:
+            key = input()
+            if key == 'capture':
+                self.increase_capture_cnt()
+            elif key == 'exit':
+                self.flag_exit = True
+    
+    def close(self):
         for device in self.devices:
             device.stop()
-        self.flag_exit = True
+        print("close device")
         return False
 
-    def space_callback(self, vis):
+    def increase_capture_cnt(self):
         os.makedirs(os.path.join(self.dirname, str(self.capture_cnt)), exist_ok=True)
         self.capture_cnt += 1
-
-        if self.capture_cnt >= 1:
-            f = open('data','extrinsic','latest.txt')
-            f.write(self.name)
-            f.close()
-        
-        return False
-
-    def merge_image(self, image_list):
-        for i in range(4):
-            image_list[i] = cv2.resize(image_list[i], (1024, 768))
-
-        tmpimg1 = np.concatenate((image_list[0],image_list[1]), axis=1)
-        tmpimg2 = np.concatenate((image_list[2],image_list[3]), axis=1)
-        img = np.concatenate((tmpimg1,tmpimg2), axis=0)
-        return img
-
-    def get_capture(self): #save image and return visualized image
-        img_list = []
-        for i in range(4):
-            capture = self.devices[i].get_capture()
-            if capture.color is None:
-                break
-            
-            img = convert_to_bgra_if_required(self.config.color_format, capture.color)
-            img_list.append(img)
-
-        if len(img_list) < 4:
-            return None
-
-        if self.capture_status < self.capture_cnt:
-            for i in range(4):
-                cv2.imwrite(os.path.join(self.dirname, str(self.capture_status), f'{i}.png'), img_list[i])
-            self.capture_status += 1
-        
-        return self.merge_image(img_list)
+        print("capture_cnt: ", self.capture_cnt)
+        return
     
+    async def send_image_to_server(self, image, url, session, device_id):
+        data = FormData()
+        data.add_field('device_id', str(device_id))
+        data.add_field('image', image, filename=f'{device_id}.png', content_type='image/png')
+        async with session.post(url, data=data) as response:
+            return await response.text()
+        
+    
+    async def send_all_images(self, img_list):
+        async with aiohttp.ClientSession() as session:
+            tasks = [asyncio.create_task(self.send_image_to_server(img, 'http://192.168.0.34/upload', session, id)) for id, img in img_list]
+            await asyncio.gather(*tasks)
+            
+    async def async_capture(self, index):
+        capture = await self.devices.get_capture()
+        if capture.color is None:
+            return None
+        id = self.ids[index]
+        return cv2.cvtColor(capture.color, cv2.COLOR_RGBA2RGB), id
+    
+    async def get_all_captures(self):
+        return await asyncio.gather(*(self.async_capture(i) for i in range(4)))
+
+    async def process_loop(self):
+        while self.flag_exit:
+            img_list = await self.get_all_captures()
+            if img_list:
+                await self.send_all_images(img_list)
+                # Optionally save images locally
+                if self.capture_status < self.capture_cnt:
+                    for id, img in id, img_list:
+                        cv2.imwrite(f'data/calib/scene{self.capture_status}/{id}.png', img)
+                    self.capture_status += 1
+                    
     def run(self):
-        glfw_key_escape = 256
-        glfw_key_space = 32
-        vis = o3d.visualization.VisualizerWithKeyCallback()
-        vis.register_key_callback(glfw_key_escape, self.escape_callback)
-        vis.register_key_callback(glfw_key_space, self.space_callback)
-        vis.create_window(f'viewer', 2048, 1536)
-        print("Sensor initialized. Press [ESC] to exit.")
-
-        vis_geometry_added = False
-        step = 0
-
-        while not self.flag_exit:
-            img = self.get_capture()
-            if img is None:
-                continue
-
-            step+= 1
-            cv2.putText(img, str(step), (512,512),cv2.FONT_HERSHEY_SIMPLEX ,3, (255, 255, 0) )
-            img = o3d.geometry.Image(img.astype(np.uint8))
-            vis.clear_geometries()
-            vis.add_geometry(img)
-                #vis_geometry_added = True
-            vis.update_geometry(img)
-            vis.poll_events()
-            vis.update_renderer()
+        loop = asyncio.get_event_loop()
+        try:
+            loop.run_until_complete(self.process_loop())
+        finally:        
+            self.close()
+            loop.close()
+    
--- src/server.py
+++ src/server.py
@@ -1,23 +1,68 @@
 from flask import Flask, request
 import os
+import numpy as np
+import cv2
+import time
+import threading
+import math
 
 app = Flask(__name__)
 
 @app.route('/upload', methods=['POST'])
-def upload_file():
-    pc_id = request.form.get('pc_id')  # Identifier for each PC
-    image = request.files['image']
+def upload_image():
+    device_id = request.form.get('device_id')
+    image = request.files.get('image')
 
-    if image:
-        directory = f"./images_from_{pc_id}"  # Folder for each PC
-        if not os.path.exists(directory):
-            os.makedirs(directory)
+    if not image:
+        return 'No image uploaded', 400
 
-        filepath = os.path.join(directory, f"{image.filename}")
-        image.save(filepath)
-        return f"Image saved from PC {pc_id}", 200
+    # You can use device_id for additional processing or naming
+    filename = f"{device_id}_{image.filename}"
+    filepath = os.path.join('uploads', filename)
 
-    return "No image found", 400
+    # Ensure the 'uploads' directory exists or create it
+    os.makedirs('uploads', exist_ok=True)
+
+    # Save the image
+    image.save(filepath)
+
+    return f'Image {filename} uploaded successfully', 200
+
+def merge_into_square_grid(image_paths):
+    images = [cv2.imread(path) for path in image_paths]
+    target_size = (3072, 4096)
+    # Determine grid size
+    grid_size = math.ceil(math.sqrt(len(images)))
+    
+    num_col = grid_size
+    num_row = (len(images) -1) // num_col + 1
+     
+    while len(images) < num_col * num_row:
+        # Add blank images if necessary to complete the square
+        images.append(np.zeros_like(images[0]))
+
+    # Resize images for consistency
+    
+    resized_images = [cv2.resize(image, (target_size[0] // num_row, target_size[1] // num_col)) for image in images]
+
+    # Create rows and merge
+    rows = [np.hstack(resized_images[i * num_col:(i + 1) * num_col]) for i in range(num_row)]
+    merged_image = np.vstack(rows)
+
+    return merged_image
+
+
+def display_images():
+    while True:
+        img = merge_into_square_grid(sorted(os.listdir('uploads')))
+        cv2.imshow('Image', img)
+        cv2.waitKey(1)  # Display for a short duration
+        time.sleep(5)  # Check the folder every 5 seconds
+
+# Start the visualization thread
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    threading.Thread(target=display_images, daemon=True).start()
+    app.run(host='192.168.0.34', port=5000, debug=True)
+
+
