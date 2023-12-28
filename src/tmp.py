04b08eb15fc8774c7064e9b01091c7ad5d5fe99c
willi19 <willi19@naver.com>
2023-12-29 01:13:24 +0900

    timestamp

--- src/tmp.py
+++ src/tmp.py
@@ -11,69 +11,74 @@ import json
 from pyk4a import PyK4A, Config, connected_device_count
 from utils import convert_to_bgra_if_required
 
+import threading
+import asyncio
+import shutil
+
 class ViewerWithCallback:
     def set_config(self, config_path):
         config_file = open(config_path, 'r')
         data = json.load(config_file)
-        self.config = Config(**data)
+        return Config(**data)
+        #self.config = Config(**data)
 
     def __init__(self):
         self.flag_exit = False
+        subordinate_config_path = 'config/pyk4a_subordinate.json'
+        subordinate_config = self.set_config(subordinate_config_path)
+
+        master_config_path = 'config/pyk4a_master.json'
+        master_config = self.set_config(master_config_path)
+
         config_path = 'config/pyk4a.json'
-        self.set_config(config_path)
+        self.config = self.set_config(config_path)
+        
+        configs = [self.config, subordinate_config, subordinate_config, master_config]
         self.device_num = connected_device_count()
-        self.devices = [PyK4A(config = self.config, device_id=device_ind) for device_ind in range(self.device_num)]
+        self.devices = [PyK4A(config = configs[device_ind], device_id=device_ind) for device_ind in range(self.device_num)]
         for i in range(self.device_num):
             self.devices[i].start()
+        self.ids = [device.serial for device in self.devices]
+        self.ids_inv = {self.ids[i]:i for i in range(self.device_num)}
                         
 
-    def escape_callback(self, vis):
-        self.flag_exit = True
-        for device in self.devices:
-            device.stop()
-        return False
+    async def async_capture(self, index):
+        id = self.ids[index]
+        capture = self.devices[index].get_capture()
+        if capture.color is None:
+            return id, None
+        img = convert_to_bgra_if_required(self.config.color_format, capture.color)
+        print(capture.color_timestamp_usec, (capture.color_system_timestamp_nsec % 10**9) // 10**6, index)
+        return id, img
 
-    def run(self):
-        glfw_key_escape = 256
-        vis = o3d.visualization.VisualizerWithKeyCallback()
-        vis.register_key_callback(glfw_key_escape, self.escape_callback)
-        vis.create_window(f'viewer', 2048, 1536)
-        print("Sensor initialized. Press [ESC] to exit.")
+    async def get_all_captures(self):
+        return await asyncio.gather(*(self.async_capture(index) for index in range(4)))
 
-        vis_geometry_added = False
+    async def process_loop(self):
         step = 0
         while not self.flag_exit:
-            tot_img = []
-            for i in range(self.device_num):
-                img = self.devices[i].get_capture()
-
-                rgbd =  img.color
-                print(img.color_system_timestamp_nsec, img.color_timestamp_usec)
-                if rgbd is None:
-                    print(i)
-                    break
-                rgbd = convert_to_bgra_if_required(self.config.color_format, rgbd)
-                rgbd = cv2.cvtColor(rgbd, cv2.COLOR_BGRA2RGB)
-                tot_img.append(cv2.resize(np.array(rgbd),(1024, 768)))
-
-            if len(tot_img) < self.device_num:
-                continue
-            
-
-            if len(tot_img) < 4:
-                for i in range(4-len(tot_img)):
-                    tot_img.append(np.zeros((768,1024,3), dtype=np.uint8))
-            tmpimg1 = np.concatenate((tot_img[0],tot_img[1]), axis=1)
-            tmpimg2 = np.concatenate((tot_img[2],tot_img[3]), axis=1)
-            img = np.concatenate((tmpimg1,tmpimg2), axis=0)
-            cv2.putText(img, str(step), (512,512),cv2.FONT_HERSHEY_SIMPLEX ,3, (255, 255, 0) )
-            img = o3d.geometry.Image(img.astype(np.uint8))
-            vis.clear_geometries()
-            vis.add_geometry(img)
-                #vis_geometry_added = True
-            vis.update_geometry(img)
-            vis.poll_events()
-            vis.update_renderer()
+            img_list = await self.get_all_captures()
+            tot_img = [None for _ in range(4)]
+            for id, img in img_list:
+                tot_img[self.ids_inv[id]] = cv2.resize(np.array(img),(1024, 768)) if img is not None else np.zeros((768,1024,3), dtype = np.uint8)
+            if img_list is not None:
+                tmpimg1 = np.concatenate((tot_img[0],tot_img[1]), axis=1)
+                tmpimg2 = np.concatenate((tot_img[2],tot_img[3]), axis=1)
+                img = np.concatenate((tmpimg1,tmpimg2), axis=0)
+                
+                cv2.imwrite(f'sync/{step}.jpg', img)
+                step += 1
+                    
+        
+    def run(self):
+        shutil.rmtree('sync')
+        os.makedirs('sync', exist_ok=True)
+        loop = asyncio.get_event_loop()
+        try:
+            loop.run_until_complete(self.process_loop())
+        finally:
+            loop.close()
+            self.close()
 
 if __name__ == "__main__":
     viewer = ViewerWithCallback()
--- src/viewer.py
+++ src/viewer.py
@@ -22,6 +22,7 @@ class ViewerWithCallback:
         config_path = 'config/pyk4a.json'
         self.set_config(config_path)
         self.device_num = connected_device_count()
+        print(f"Found {self.device_num} connected devices")
         self.devices = [PyK4A(config = self.config, device_id=device_ind) for device_ind in range(self.device_num)]
         for i in range(self.device_num):
             self.devices[i].start()
