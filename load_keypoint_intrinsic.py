0b2682014bf7cdc583f2b4583070d0e39fb6fab8
willi19 <willi19@naver.com>
2024-01-03 01:20:38 +0900

    load_keypoint_intrinsic with debug

--- load_keypoint_intrinsic.py
+++ load_keypoint_intrinsic.py
@@ -35,8 +35,48 @@ if __name__ == "__main__":
     keypoint_list, objpoint_list = load_checkpoint('data', debug=False)
     os.makedirs('intrinsic', exist_ok=True)
     for cam_name, kypt in keypoint_list.items():
+        data = load_json(os.path.join('data', 'intrinsic_kinect', '0102214559','json',cam_name + '.json'))
+        for camparam in data['CalibrationInformation']['Cameras']:
+            if camparam['Purpose'] == 'CALIBRATION_CameraPurposePhotoVideo':
+                param = camparam['Intrinsics']['ModelParameters']
+
+        x = 4096
+        y = 3072
+
+        cx = param[0] * x
+        cy = param[1] * y
+        fx = param[2] * x
+        fy = param[3] * y
+        k1 = param[4]
+        k2 = param[5]
+        k3 = param[6]
+        k4 = param[7]
+        k5 = param[8]
+        k6 = param[9]
+        p2 = param[12]
+        p1 = param[13]
+        
+        print(cam_name, "--------------------")
+        print(fx, fy, cx, cy, k1, k2, k3, k4, k5, k6, p1, p2)
+        
+        #ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(objpoint_list[cam_name], kypt, (4096, 3072), None, None, flags = cv2.CALIB_RATIONAL_MODEL)
+        #print(mtx[0][0], mtx[1][1], mtx[0][2], mtx[1][2], dist[0][0], dist[0][1], dist[0][4], dist[0][5], dist[0][6], dist[0][7], dist[0][2], dist[0][3])
+        
+        cam_mtx = np.array([[fx, 0, cx], [0, fy, cy], [0, 0, 1]])
+        dist = np.array([[k1, k2, p1, p2, k3, k4, k5, k6]])
+
         ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(objpoint_list[cam_name], kypt, (4096, 3072), None, None, flags = cv2.CALIB_RATIONAL_MODEL)
-        os.makedirs(os.path.join('intrinsic', cam_name), exist_ok=True)
-        np.save(os.path.join('intrinsic', cam_name, 'mtx.npy'), mtx)
-        np.save(os.path.join('intrinsic', cam_name, 'dist.npy'), dist)
+        print(ret, mtx[0][0], mtx[1][1], mtx[0][2], mtx[1][2], dist[0][0], dist[0][1], dist[0][4], dist[0][5], dist[0][6], dist[0][7], dist[0][2], dist[0][3])
+
+        cam_mtx = np.array([[fx, 0, cx], [0, fy, cy], [0, 0, 1]])
+        dist = np.array([[k1, k2, p1, p2, k3, k4, k5, k6]])
+
+        ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(objpoint_list[cam_name], kypt, (4096, 3072), cam_mtx, dist, flags = cv2.CALIB_RATIONAL_MODEL+cv2.CALIB_USE_INTRINSIC_GUESS)
+        print(ret, mtx[0][0], mtx[1][1], mtx[0][2], mtx[1][2], dist[0][0], dist[0][1], dist[0][4], dist[0][5], dist[0][6], dist[0][7], dist[0][2], dist[0][3])
+        
+        cam_mtx = np.array([[fx, 0, cx], [0, fy, cy], [0, 0, 1]])
+        dist = np.array([[k1, k2, p1, p2, k3, k4, k5, k6]])
+        flags = cv2.CALIB_RATIONAL_MODEL | cv2.CALIB_FIX_K1 | cv2.CALIB_FIX_K2 | cv2.CALIB_FIX_K3 | cv2.CALIB_FIX_K4 | cv2.CALIB_FIX_K5 | cv2.CALIB_FIX_K6 | cv2.CALIB_FIX_FOCAL_LENGTH | cv2.CALIB_FIX_PRINCIPAL_POINT | cv2.CALIB_USE_INTRINSIC_GUESS | cv2.CALIB_FIX_TANGENT_DIST
+        print(flags)
+        ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(objpoint_list[cam_name], kypt, (4096, 3072), cam_mtx, dist, flags = flags)
         print(ret, mtx[0][0], mtx[1][1], mtx[0][2], mtx[1][2], dist[0][0], dist[0][1], dist[0][4], dist[0][5], dist[0][6], dist[0][7], dist[0][2], dist[0][3])
