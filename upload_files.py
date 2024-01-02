e301035184c14cd2600a0dd0bdf9e1b82a7c69be
willi19 <willi19@naver.com>
2024-01-02 17:44:09 +0900

    add file uploading

---
+++ upload_files.py
@@ -0,0 +1,29 @@
+import os
+import shutil
+
+def copy_directory_with_ignore_existing(source_path, destination_path):
+    """
+    Copy the contents of the source directory to the destination directory.
+    If a file or directory already exists in the destination, it will be ignored.
+
+    :param source_path: Path to the source directory.
+    :param destination_path: Path to the destination directory.
+    """
+    for item in os.listdir(source_path):
+        s = os.path.join(source_path, item)
+        d = os.path.join(destination_path, item)
+        if os.path.isdir(s):
+            if not os.path.exists(d):
+                shutil.copytree(s, d)
+            else:
+                copy_directory_with_ignore_existing(s, d)
+        else:
+            if not os.path.exists(d):
+                shutil.copy2(s, d)
+
+# Example usage
+source_directory = "data"
+destination_directory = "shared_data"
+
+# Uncomment the line below to actually copy the directory
+# copy_directory_with_ignore_existing(source_directory, destination_directory)
